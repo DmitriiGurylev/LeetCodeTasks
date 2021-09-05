ccaa189911665aa1e1aa8951052a1171375da23c
Dmitry Gurylev <gurylev.dmitrii@mail.ru>
2021-09-05 15:08:12 +0300

    arrays

--- Main/src/Task9.java
+++ Main/src/Task9.java
@@ -5,9 +5,10 @@ import java.util.*;
 public class Task9 {
 
     public static void main (String[] args){
-        int[] arr = {3,2,1,2};
+        int[] arr = {7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
         System.out.println(thirdMax(arr));
         System.out.println(findDisappearedNumbers(arr));
+        System.out.println(Arrays.toString(plusOne(arr)));
 
     }
 
@@ -52,5 +53,50 @@ public class Task9 {
         Arrays.sort(nums);
 
         return nums;
+    }
+
+    public static int[] plusOne(int[] digits) {
+        String str = "";
+        for (int value : digits){
+            str = str + value;
+        }
+        String newStr = "";
+        if (str.charAt(str.length()-1) != '9'){
+            for (int i=0; i<str.length()-1; i++){
+                newStr += str.charAt(i);
+            }
+            newStr += Integer.parseInt(str.substring(str.length()-1)) + 1;
+        }
+        else{
+            boolean сharNotNine = false;
+            int index=0;
+            for (int i=str.length()-1; i>=0; i--){
+                if (str.charAt(i) !='9'){
+                    сharNotNine = true;
+                    index = i;
+                    break;
+                }
+            }
+            if (!сharNotNine){
+                newStr += 1;
+                for (int i=0; i<str.length(); i++){
+                    newStr += 0;
+                }
+            }
+            else{
+                for (int i=0; i<index; i++){
+                    newStr += str.charAt(i);
+                }
+                newStr += Integer.parseInt(str.substring(index,index+1)) + 1;
+                for (int i=index+1; i<str.length(); i++){
+                    newStr += 0;
+                }
+            }
+        }
+        int[] arrNew = new int[newStr.length()];
+        for (int i=0; i< arrNew.length; i++){
+            arrNew[i] = Integer.parseInt(newStr.substring(i,i+1));
+        }
+        return arrNew;
     }
 }
